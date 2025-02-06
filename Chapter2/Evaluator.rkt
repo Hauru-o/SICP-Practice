9ef4418b6de4b739fe14c3ffe1c7c5422422c367
Hauru-o <yu_xiafou@outlook.com>
2025-02-06 13:57:30 +0800

    Evaluator 'lookup'

--- Chapter2/Evaluator.rkt
+++ Chapter2/Evaluator.rkt
@@ -12,3 +12,60 @@
           (else
            (apply (eval (car exp) env)
                   (evlist (cdr exp) env))))))
+
+(define apply
+  (lambda (proc args)
+    (cond ((primitive? proc)
+           (apply-primop proc args))
+          ((eq? (car proc) 'closure)    ; (closure ((x) (+ x y)) <env>)
+           (eval (cadadr proc)
+                 (bind (caadr proc)
+                       args
+                       (caddr proc))))
+          (else error))))
+
+(define evlist
+  (lambda (L evn)
+    (cond ((eq? L '()) '())
+          (else
+           (cons (eval (car L) env)
+                 (evlist (cdr l) env))))))
+
+(define evcond
+  (lambda (clauses env)
+    (cond ((eq? clauses '()) '())
+          ((eq? (caar clauses) 'else)
+           (eval (cadar clauses) env))
+          ((false? (eval (caar clauses) env))
+           (evcond (cdr clauses) env))
+          (else
+           (eval (cadar clauses) env)))))
+
+(define bind
+  (lambda (vars vals env)
+    (cons (pair-up vars vals)
+          env)))
+
+(define pair-up
+  (lambda (vars vals)
+    (cond
+      ((eq? vars '())
+       (cond ((eq? vals '()) '())
+             (else (error TMA))))
+      ((eq? vals '()) (error TFA))
+      (else
+       (cons (cons (car vars)
+                   (car vals))
+             (pair-up (cdr vars)
+                      (cdr vals)))))))
+
+(define lookup
+  (lambda (sym env)
+    (cond ((eq? env '()) (error UBV))
+          (else
+           ((lambda (vcell)
+              (cond ((eq? vcell '())
+                     (lookup sym
+                             (cdr env)))
+                    (else (cdr vcell))))
+            (assq sym (car env)))))))
