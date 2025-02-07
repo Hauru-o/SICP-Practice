b07ae389d125c2193ba92e402a31fe5fb8a37643
Hauru-o <yu_xiafou@outlook.com>
2025-02-07 14:01:21 +0800

    pair-up symbol?

--- src/Evaluator.rkt
+++ src/Evaluator.rkt
@@ -52,6 +52,8 @@
       ((eq? vars '())
        (cond ((eq? vals '()) '())
              (else (error TMA))))
+      ((symbol? vars)
+       (cons (cons vars vals) '()))
       ((eq? vals '()) (error TFA))
       (else
        (cons (cons (car vars)
