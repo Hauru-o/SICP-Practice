e77b5d1bf92ff09cd8bf4e8ecc645e1ea9a58c5e
Hauru-o <yu_xiafou@outlook.com>
2025-02-13 13:21:52 +0800

    Interpreter By Lua

--- src/Interpreter.lua
+++ src/Interpreter.lua
@@ -10,6 +10,14 @@ local function cdr(pair)
 	return pair[2]
 end
 
+local function cadr(x)
+	return car(cdr(x))
+end
+
+local function caddr(x)
+	return car(cdr(cdr(x)))
+end
+
 local function list(...)
 	local args = { ... }
 	local result = {}
@@ -48,4 +56,44 @@ local env0 = list(frame0)
 
 local function apply_primop(op, args)
 	return op(car(args), cdr(args))
+end
+
+local function oprator(exp)
+	return car(exp)
+end
+
+local function isNumber(exp)
+	if type(exp) == "number" then
+		return true
+	end
+	return false
+end
+
+local function isSymbol(exp)
+	if type(exp) == "string" then
+		return true
+	end
+	return false
+end
+
+local function eval(exp, env)
+	if isNumber(exp) then
+		return exp
+	elseif isSymbol(exp) then
+		return lookup(exp, env)
+	elseif oprator(exp) == 'quote' then
+		return cadr(exp)
+	elseif oprator(exp) == 'lambda' then
+		return list('closure', cdr(exp), env)
+	elseif oprator(exp) == 'define' then
+		return evdefine(cadr(exp), eval(caddr(exp), env), env)
+	elseif oprator(exp) == 'cond' then
+		return evcond(cdr(exp), env)
+	elseif oprator(exp) == 'begin' then
+		return evbegin(cdr(exp), env)
+	else
+		return apply(
+			eval(car(exp), env),
+			evlist(cdr(exp), env))
+	end
 end
